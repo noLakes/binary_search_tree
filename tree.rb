df3a9c95152b8be7a24b01b379f7e9d1ac27cc1e
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-06-04 19:05:20 -0400

    rewrote DFS methods to be recursive w/yield and smaller

--- tree.rb
+++ tree.rb
@@ -3,7 +3,7 @@ class Node
   include Comparable
   attr_accessor :val, :left_child, :right_child
 
-  def initialize(val=nil)
+  def initialize(val = nil)
     @val = val
     @left_child = nil
     @right_child = nil
@@ -20,23 +20,21 @@ end
 
 class Tree
   attr_reader :root
-  def initialize(arry=[nil])
+  def initialize(arry = [nil])
     @root = build_tree(arry)
   end
 
   def build_tree(arry)
+    return nil unless arry[0]
     arry.uniq!
     new_tree = Node.new(arry.shift)
-    
-    return new_tree if arry.length == 0 
-
     arry.each do |item|
       insert(item, new_tree)
     end
     new_tree
   end
 
-  def insert(val, root=@root)
+  def insert(val, root = @root)
     if val < root.val
       root.left_child.nil? ? root.left_child = Node.new(val) : insert(val, root.left_child)
     elsif val > root.val
@@ -48,7 +46,7 @@ class Tree
 
   end
 
-  def find(val, read=@root)
+  def find(val, read = @root)
     if val == read.val
       return read
     elsif read.end?
@@ -62,55 +60,40 @@ class Tree
     end
   end
 
-  def pre_order(node=@root)
-    stack = []
-    output = []
-    read = node
-    stack.push(read)
-    while !stack.empty?
-      read = stack.shift
-      stack.push(read.left_child) unless read.left_child.nil?
-      stack.push(read.right_child) unless read.right_child.nil?
-      output.push(read.val)
-    end
-    output
+  #Root Left Right
+  def pre_order(node=@root, result = [])
+    return if node.nil?
+      block_given? ? yield(node) : result << node.val
+      pre_order(node.left_child, result)
+      pre_order(node.right_child, result)
+    return result
   end
 
-  def in_order(node=@root)
-    stack = []
-    output = []
-    read = node
-    while (read || !stack.empty?)
-      while read
-        stack.push(read)
-        read = read.left_child
-      end
-      read = stack.pop
-      output.push(read.val)
-      read = read.right_child
-    end
-    output
+  #Left Root Right
+  def in_order(node = @root, result = [])
+    return if node.nil?
+      in_order(node.left_child, result)
+      block_given? ? yield(node) : result << node.val
+      in_order(node.right_child, result)
+    return result
   end
 
+  #Right Root Left
   def reverse_order(node=@root)
-    stack = []
-    output = []
-    read = node
-    while (read || !stack.empty?)
-      while read
-        stack.push(read)
-        read = read.right_child
-      end
-      read = stack.pop
-      output.push(read.val)
-      read = read.left_child
-    end
-    output
+    return if node.nil?
+      reverse_order(node.right_child, result)
+      block_given? ? yield(node) : result << node.val
+      reverse_order(node.left_child, result)
+    return result
   end
-
+  
   #Left Right Root
-  def post_order
-
+  def post_order(node = @root, result = [])
+    return if node.nil?
+      post_order(node.left_child, result)
+      post_order(node.right_child, result)
+      block_given? ? yield(node) : result << node.val
+    return result
   end
 
   def level_order
@@ -120,6 +103,10 @@ class Tree
 end
 
 x = Tree.new([1, 7, 4, 23, 8, 9, 3, 5, 67, 6345, 324])
-puts x.in_order.join(' ')
-puts x.reverse_order.join(' ')
-puts x.pre_order.join(' ')
+
+puts "pre-order"
+p x.pre_order
+puts "\nin-order"
+p x.in_order
+puts "\npost-order"
+p x.post_order
