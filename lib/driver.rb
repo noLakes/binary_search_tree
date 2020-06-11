a1550908aea2e15e0da7956b1c27396b3e910376
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-06-10 20:07:17 -0400

    finished driver script. final commit (for now)

--- lib/driver.rb
+++ lib/driver.rb
@@ -7,7 +7,6 @@ tree = Tree.new(test_array)
 puts "\nnew tree made from random test array: #{test_array}"
 
 puts "\ntree balanced? #{tree.balanced?}"
-puts tree.weight
 puts "\nlevel order:\n#{tree.level_order}"
 puts "\nlevel order with depth: #{tree.level_order_with_depth}"
 puts "\npreorder: #{tree.pre_order}"
@@ -15,10 +14,21 @@ puts "\npost order: #{tree.post_order}"
 puts "\nin order: #{tree.in_order}"
 puts "\nadding several small numbers to imbalance array (1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13)"
 [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13].each {|num| tree.insert(num)}
-#puts "\nlevel order with depth: #{tree.level_order_with_depth}"
-puts tree.weight
-puts tree.depth(tree.find(13))
 puts "\ntree balanced? #{tree.balanced?}"
-#puts "\ntree balance is now: #{tree.balanced?} "
+puts "\nattempting to rebalance..."
 
+3.times do
+  sleep 0.8
+  puts "..."
+end
 
+tree.rebalance
+puts "\ncomplete!"
+puts "\ntree balanced? #{tree.balanced?}"
+puts "\nlevel order:\n#{tree.level_order}"
+puts "\nlevel order with depth: #{tree.level_order_with_depth}"
+puts "\npreorder: #{tree.pre_order}"
+puts "\npost order: #{tree.post_order}"
+puts "\nin order: #{tree.in_order}"
+sleep 0.8
+puts "\ntesting complete!\nthank you for your patience"
